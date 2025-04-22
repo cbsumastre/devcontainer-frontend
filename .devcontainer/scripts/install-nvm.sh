a4cfd723f085d7c378797f1147a15714d06b973b
Carlos Blázquez <admin@firstflip.es>
2025-04-22 15:47:29 +0200

    refact

--- .devcontainer/scripts/install-nvm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
-
-source /home/vscode/.nvm/nvm.sh
-
-nvm install 22
---
+++ .devcontainer/scripts/post-create-command.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+
+wget -qO- https://get.pnpm.io/install.sh | ENV="/home/vscode/.bashrc" SHELL="$(which bash)" bash -
+
+curl -fsSL https://bun.sh/install | bash
+
+source /home/vscode/.nvm/nvm.sh
+
+nvm install 22
+
+npm install -g yarn
